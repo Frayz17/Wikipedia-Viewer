2f6cdf9ec6cf32e82e7943f7cc58b21442c63616
Andrey Dmitriew <andrydmit@gmail.com>
2017-09-11 20:23:18 +0300

    Add random button with functionality.

--- js/common.js
+++ js/common.js
@@ -1,6 +1,13 @@
-var button = document.getElementById('search-query-submit-id');
+var buttonSearch = document.getElementById('button-search-id');
+var buttonRandom = document.getElementById('button-random-id')
 var testParagraph = document.getElementById('test');
 var searchString = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=java"
+
+
+// random wiki article when random button pressed
+buttonRandom.addEventListener("click", function(event) {
+	window.open("https://en.wikipedia.org/wiki/Special:Random");
+})
 
 button.addEventListener("click", function(event) {
 	testParagraph
