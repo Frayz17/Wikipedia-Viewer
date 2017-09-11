a4168b2953cc5f3f6cd732b49faa83025cc0b1c5
unknown <C:\Users\Alex\AppData\Roaming\The Bat!>
2017-09-11 16:55:35 +0300

    creating JSON function

---
+++ js/common.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('search-query-submit-id');
+var testParagraph = document.getElementById('test');
+var searchString = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=java"
+
+button.addEventListener("click", function(event) {
+	testParagraph
+});
+
+
+// "/w/api.php?action=opensearch&format=json&search=java"
+// "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=java"
